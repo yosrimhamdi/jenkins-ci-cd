f49bc02871d6599dbffcfdb51baf49f6147cbb23
Yosri Mhamdi <yosri@mhamdi.co>
2022-08-12 21:44:10 +0100

    Added a dsl.

---
+++ nodejs.groovy
@@ -0,0 +1,17 @@
+job('NodeJS example') {
+    scm {
+        git('https://github.com/yosrimhamdi/jenkins-ci-cd') {
+            node / gitConfigName('Yosri Mhamdi')
+            node / gitConfigEmail('yosri@mhamdi.co')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs')
+    }
+    steps {
+        shell("npm install")
+    }
+}

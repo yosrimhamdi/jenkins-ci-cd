be396274c3f0ad987a165404da05bb8cde218fac
Yosri Mhamdi <yosri@mhamdi.co>
2022-08-12 22:36:06 +0100

    added a docker job.

--- jobs/test.groovy
+++ jobs/test.groovy
@@ -14,7 +14,5 @@ job('node-app') {
     steps {
         shell("npm install")
         shell("npm test")
-        shell("docker build -t yosrimhamdi/first-demo --pull=true /var/lib/jenkins/workspace/node-app")
-        shell("docker push yosrimhamdi/first-demo:latest")
     }
 }
